9e883346cbba29fb556c42970c0f2f3a98aa0168
rcabralc <rcabralc@gmail.com>
2016-03-09 16:16:12 -0300

    Take less space

--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -1,28 +1,35 @@
 function fish_right_prompt
+    set sep ""
+
     set rubyversion (rbenv version-name)
     if test $status -eq 0
         if test $rubyversion != 'system'
             set_color grey
             printf '%s ' $rubyversion
+            set sep " "
         end
     else
         set_color -o red
         printf 'bad Ruby '
+        set sep " "
     end
     set_color normal
 
     set_color bryellow
-    printf '%s ' (date "+%d/%m")
+    printf "$sep%s" (date "+%d/%m")
     set_color normal
+    set sep " "
 
     set_color brown
-    printf '%s ' (date "+%Hh%M")
+    printf "$sep%s" (date "+%Hh%M")
     set_color normal
+    set sep " "
 
     set batteryprompt (battery_prompt)
     if test $status -eq 0
         set_color red
-        printf '⚡%s' $batteryprompt
+        printf "$sep⚡%s" $batteryprompt
         set_color normal
+        set sep " "
     end
 end
