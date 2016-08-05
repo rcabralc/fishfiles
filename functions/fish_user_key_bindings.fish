1e7da528ddb4aa4f542711e623ca9456c93acd38
Rafael Cabral Coutinho <rcabralc@gmail.com>
2016-08-05 00:05:51 -0300

    Add some missing bindings

--- functions/fish_user_key_bindings.fish
+++ functions/fish_user_key_bindings.fish
@@ -2,4 +2,8 @@ function fish_user_key_bindings
     fish_vi_key_bindings
 
     bind -M insert \ej accept-autosuggestion end-of-line execute
+    bind -M insert \cx accept-autosuggestion end-of-line
+    bind -M insert \cp up-or-search
+    bind -M insert \cn down-or-search
+    bind -M insert \cf forward-word
 end
