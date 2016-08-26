23c50441d62d0c87ed55fd5ce9c156feb870db23
Rafael Cabral Coutinho <rcabralc@gmail.com>
2016-08-26 11:34:47 -0300

    Ease suggestion in favor of difficulting completion

--- functions/fish_user_key_bindings.fish
+++ functions/fish_user_key_bindings.fish
@@ -1,9 +1,10 @@
 function fish_user_key_bindings
     fish_vi_key_bindings
 
-    bind -M insert \ej accept-autosuggestion end-of-line execute
-    bind -M insert \cx accept-autosuggestion end-of-line
     bind -M insert \cp up-or-search
     bind -M insert \cn down-or-search
+
+    bind -M insert \cx complete
+    bind -M insert \t accept-autosuggestion end-of-line
     bind -M insert \cf forward-word
 end
