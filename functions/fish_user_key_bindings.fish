a55e88e0184a900da10310cabbeb1c3af10e0651
Rafael Cabral Coutinho <rafael.coutinho@biva.com.br>
2016-12-08 13:53:28 -0200

    Swap completion and suggestion bindings

--- functions/fish_user_key_bindings.fish
+++ functions/fish_user_key_bindings.fish
@@ -4,7 +4,6 @@ function fish_user_key_bindings
     bind -M insert \cp up-or-search
     bind -M insert \cn down-or-search
 
-    bind -M insert \co complete
-    bind -M insert \t accept-autosuggestion end-of-line
+    bind -M insert \co accept-autosuggestion end-of-line
     bind -M insert \cf forward-word
 end
