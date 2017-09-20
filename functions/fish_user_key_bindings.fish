e7736ac65a0b92bd88b39d49553030bb7e54121b
Rafael Cabral Coutinho <rafael.coutinho@biva.com.br>
2017-09-20 20:07:23 -0300

    Bind control-l to accept suggestion

--- functions/fish_user_key_bindings.fish
+++ functions/fish_user_key_bindings.fish
@@ -4,10 +4,6 @@ function fish_user_key_bindings
     bind -M insert \cp up-or-search
     bind -M insert \cn down-or-search
 
-    bind -M insert \co accept-autosuggestion end-of-line
+    bind -M insert \cl accept-autosuggestion end-of-line
     bind -M insert \cf forward-word
-
-    bind -e \cl
-    bind -e -M insert \cl
-    bind -e -M visual \cl
 end
