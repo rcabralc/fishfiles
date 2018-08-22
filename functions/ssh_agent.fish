21faddc6c815eb7fe5976b4f742b3c3e884c8fb0
Rafael Cabral Coutinho <rcabralc@gmail.com>
2018-08-22 00:02:05 -0300

    Don't show agent pid twice
    ssh-agent -c already prints agent PID.

--- functions/ssh_agent.fish
+++ functions/ssh_agent.fish
@@ -9,7 +9,6 @@ function ssh_agent --description 'launch the ssh-agent and add identities'
     if test $status -eq 2
         # Agent is not running or is not accessible.
         eval (command ssh-agent -c | sed 's/setenv/set -Ux/')
-        echo "ssh-agent has pid $SSH_AGENT_PID"
     end
 
     for identity in (ls ~/.ssh/*.pub | sed 's/\.pub$//')
