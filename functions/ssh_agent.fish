0bcd75dc9206003205a813ff515ca24e2f66945e
Rafael Cabral Coutinho <rcabralc@gmail.com>
2018-08-21 23:28:50 -0300

    Keep un-shadowing of global agent vars
    If the environment inherits these vars but the agent is actually not
    running, a new agent would be created everytime a fish shell is spawned
    within this environment.
    
    An agent started by the window manager will be ignored, but at least it
    will be empty forever (just a small waste)

--- functions/ssh_agent.fish
+++ functions/ssh_agent.fish
@@ -1,14 +1,13 @@
 function ssh_agent --description 'launch the ssh-agent and add identities'
     command -v pass >/dev/null; or return
 
+    # Un-shadow universal variables
+    set -ge SSH_AGENT_PID
+    set -ge SSH_AUTH_SOCK
+
     ssh-add -l >/dev/null 2>/dev/null
     if test $status -eq 2
         # Agent is not running or is not accessible.
-
-        # Un-shadow universal variables
-        set -ge SSH_AGENT_PID
-        set -ge SSH_AUTH_SOCK
-
         eval (command ssh-agent -c | sed 's/setenv/set -Ux/')
         echo "ssh-agent has pid $SSH_AGENT_PID"
     end
